c77d663a8327a25d151e2d6a9d98d75f118abf22
Joseph Williams <bobapple761@gmail.com>
2026-09-09 00:02:54 -0400

    Added SQLite for persistence for task

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
+import sqlite3
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from enum import Enum
 from datetime import datetime
+from sqlite3 import connect 
 
 app = FastAPI()
-tasks = []
 
 class TaskCategory(str, Enum):
     SCHOOL = "school"
@@ -21,14 +22,48 @@ class Task(BaseModel):
     cognitive_demand: int = Field(ge=1, le=5)
     completed : bool = False
 
+def get_db_connection():
+    connection = sqlite3.connect("planner.db")
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def init_db():
+    connection= get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT NOT NULL,
+        category TEXT NOT NULL,
+        due_at TEXT NOT NULL,
+        estimated_minutes INTEGER NOT NULL,
+        cognitive_demand INTEGER NOT NULL,
+        completed INTEGER NOT NULL DEFAULT 0
+        )""")
+    connection.commit()
+    connection.close()
+
+init_db()
 
 @app.get("/")
 def root():
     return {"message": "Energy-Aware Planner API"}
 @app.post("/tasks")
 def create_task(task: Task):
-    tasks.append(task)
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute(
+        "INSERT INTO tasks (title, description, category, due_at, estimated_minutes, cognitive_demand, completed) VALUES (?, ?, ?, ?, ?, ?, ?)",
+        (task.title, task.description, task.category.value, task.due_at.isoformat(), task.estimated_minutes, task.cognitive_demand, int(task.completed))
+    )
+    connection.commit()
+    connection.close()
     return {"message": "Task created successfully", "task": task}
 @app.get("/tasks")
 def get_tasks():
-    return tasks
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor .execute("SELECT * FROM tasks")
+    rows = cursor.fetchall()
+    connection.close()
+    return [dict(row) for row in rows]
